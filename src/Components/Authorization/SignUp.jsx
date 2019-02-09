d631e6b960f76c8093154e94d79f60e3d44e2777
Taylor S. Rotolo <tayrothepro@gmail.com>
2019-02-09 16:54:59 -0600

    test change

--- src/Components/Authorization/SignUp.jsx
+++ src/Components/Authorization/SignUp.jsx
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import {
-    Form, Icon, Input, Button, Checkbox, Card
+    Form, Icon, Input, Button, Checkbox, Card, Select, Switch, Row, Col
 } from 'antd';
 import FormItem from 'antd/lib/form/FormItem';
 import '../../Styling/AuthorizationWrapper.css'
+
+const { Option } = Select;
 
 class SignUpForm extends Component {
     constructor(props) {
@@ -12,27 +14,34 @@ class SignUpForm extends Component {
 
         }
     }
+    /* hello */
     render() {
         return (
             <div className="sign-up">
-                <Card style={{width: 350}}>
+                <Card style={{ width: 350 }}>
                     <h3>Sign Up</h3>
                     <Form className="">
                         <Form.Item>
-                           <Input placeholder="Name" Icon="email"/> 
+                            <Input placeholder="Name" Icon="email" required />
                         </Form.Item>
                         <Form.Item>
-                           <Input placeholder="Email" Icon="email"/> 
+                            <Input placeholder="Email" Icon="email" required />
                         </Form.Item>
                         <Form.Item>
-                           <Input placeholder="Password" Icon="email"/> 
+                            <Input placeholder="Password" Icon="email" required />
+                        </Form.Item>
+                        <Form.Item>
+                            <Select placeholder="Please select an Account Type" required>
+                                <Option value="">Driver</Option>
+                                <Option value="">Customer</Option>
+                            </Select>
                         </Form.Item>
                         <div className="sign-up-button">
-                        <Button type="primary">Sign Up</Button>
+                            <Button type="primary">Sign Up</Button>
                         </div>
                     </Form>
                 </Card>
-            </div>
+            </div >
         )
     }
 }
