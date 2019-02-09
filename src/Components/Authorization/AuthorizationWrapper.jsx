e8a41f097b282d59c45cb495d2827f959074406a
Taylor S. Rotolo <tsrotolo@go.olemiss.edu>
2019-02-08 22:09:32 -0600

    set up

--- src/Components/Authorization/AuthorizationWrapper.jsx
+++ src/Components/Authorization/AuthorizationWrapper.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Button } from 'antd';
-import "../Styling/AuthorizationWrapper.css"
+import "../../Styling/AuthorizationWrapper.css"
 
 class AuthorizationWrapper extends Component {
     constructor(props) {
--- src/index.js
+++ src/index.js
@@ -2,16 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import HttpsRedirect from 'react-https-redirect';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import AuthorizationWrapper from './Components/AuthorizationWrapper';
+import AuthorizationWrapper from './Components/Authorization/AuthorizationWrapper';
 
 ReactDOM.render(
+    <HttpsRedirect>
         <BrowserRouter>
             <Switch>
                 <Route path="/authorization" component={AuthorizationWrapper}/>
             </Switch>
         </BrowserRouter>
+    </ HttpsRedirect>
     , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
