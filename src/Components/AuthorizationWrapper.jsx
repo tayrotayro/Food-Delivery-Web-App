e9839d7728fec42befa0d54cab28240bf9f78bfa
Taylor S. Rotolo <tsrotolo@go.olemiss.edu>
2019-02-08 21:54:52 -0600

    Initial commit final

---
+++ src/Components/AuthorizationWrapper.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { Button } from 'antd';
+import "../Styling/AuthorizationWrapper.css"
+
+class AuthorizationWrapper extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            
+        }
+    }
+    render() {
+        return(
+            <div className="authorization-wrapper">
+                <div className="image-column">
+                    
+                </div>
+                <div className="login-column">
+                    <Button type="primary">Primary</Button>
+                </div>
+            </div>
+        )
+
+    }
+}
+
+export default AuthorizationWrapper;
