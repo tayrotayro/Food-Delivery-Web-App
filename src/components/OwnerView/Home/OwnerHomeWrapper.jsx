6ab44e2aba158153af84478229335a08e950cfcc
Taylor S. Rotolo <tayrothepro@gmail.com>
2019-04-03 01:47:45 -0500

    created a lot of stuff in create restaurant and owner home

--- src/components/OwnerView/Home/OwnerHomeWrapper.jsx
+++ src/components/OwnerView/Home/OwnerHomeWrapper.jsx
@@ -1,19 +1,44 @@
 import React, { Component } from 'react';
 import { Divider } from 'antd';
+import axios from 'axios';
 
 class OwnerHomeWrapper extends Component {
     constructor(props) {
         super(props);
         this.state = {
-
+            restaurants: []
         }
+    }
+
+    componentDidMount() {
+        const baseUserId = localStorage.getItem('loggedInUserId');
+
+        axios.get(`http://localhost:5000/api/restaurant/${baseUserId}`)
+            .then(response => {
+                this.setState({
+                    restaurants: response.data.data
+                })
+            })
+            .catch(err => {
+                console.log(err);
+            })
     }
 
     render() {
         return (
             <div className="owner-home">
-            <h1>Owner Home</h1>
-            <Divider >My Restaurants</Divider>
+                <h1>Owner Home</h1>
+                <Divider >My Restaurants</Divider>
+                {
+                    this.state.restaurants.map(restaurant => {
+                        return (
+                            <div>
+                                <h1>{restaurant.name}</h1>
+                                <h3>{restaurant.phone}</h3>
+                            </div>
+                        )
+                    })
+                }
             </div>
         )
     }
