40a2a97a61f624f7096d33ea55569d3bd1b32782
Taylor S. Rotolo <tayrothepro@gmail.com>
2019-04-03 23:31:56 -0500

    almost completed owener home page

--- src/components/OwnerView/Home/OwnerHomeWrapper.jsx
+++ src/components/OwnerView/Home/OwnerHomeWrapper.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { Divider } from 'antd';
+import { Divider, Card, Menu, Dropdown, Icon } from 'antd';
 import axios from 'axios';
+import "./style.css";
 
 class OwnerHomeWrapper extends Component {
     constructor(props) {
@@ -14,7 +15,8 @@ class OwnerHomeWrapper extends Component {
         const baseUserId = localStorage.getItem('loggedInUserId');
 
         axios.get(`http://localhost:5000/api/restaurant/${baseUserId}`)
-            .then(response => {
+        .then(response => {
+            console.log(response);
                 this.setState({
                     restaurants: response.data.data
                 })
@@ -24,7 +26,22 @@ class OwnerHomeWrapper extends Component {
             })
     }
 
+
+    //use card to render info
+
+
     render() {
+        const { Meta } = Card;
+        const menu = (
+            <Menu style={{width: 200, textAlign: "center"}}>
+                <h3>Restaurant Info</h3>
+                {/* <h3>Addresss:</h3> <h5>{this.state.restaurants.phone}</h5> */}
+                <Menu.Divider />
+                <Menu.Item key="3">3rd menu item</Menu.Item>
+            </Menu>
+        );
+
+
         return (
             <div className="owner-home">
                 <h1>Owner Home</h1>
@@ -32,9 +49,25 @@ class OwnerHomeWrapper extends Component {
                 {
                     this.state.restaurants.map(restaurant => {
                         return (
-                            <div>
-                                <h1>{restaurant.name}</h1>
-                                <h3>{restaurant.phone}</h3>
+                            <div className="restaurant-cards">
+                                <Card
+                                    hoverable
+                                    style={{ width: 240, textAlign: "center" }}
+                                    cover={<img alt="example" src={restaurant.pictureURL} />}
+                                >
+                                    <Divider />
+                                    <Meta
+                                        title={restaurant.name}
+                                        description={restaurant.description}
+                                    />
+                                    <div className="dropdown" >
+                                        <Dropdown overlay={menu} trigger={['click']}>
+                                            <a className="ant-dropdown-link" href="#">
+                                               More Info <Icon type="down" />
+                                            </a>
+                                        </Dropdown>
+                                    </div>
+                                </Card>
                             </div>
                         )
                     })
@@ -42,6 +75,7 @@ class OwnerHomeWrapper extends Component {
             </div>
         )
     }
+
 }
 
 export default OwnerHomeWrapper;
